f566f9c8bcfa04fbd678d7a6613b97c38706ac2c
이유준 사원 <yjlee@seowoninfo.com>
2022-02-25 23:16:34 +0900

    임시 커밋

---
+++ src/components/TheProjectList.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: "TheProjectList",
+};
+</script>
+
+<style></style>
---
+++ src/components/TheProjectListItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card class="mx-auto my-12 project-item-card" max-width="374">
+    <v-img
+      height="240"
+      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+    ></v-img>
+    <v-card-title>Cafe Badilico</v-card-title>
+
+    <v-card-text>
+      <v-row align="center" class="mx-0">
+        <v-rating
+          :value="4.5"
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ms-4">4.5 (413)</div>
+      </v-row>
+
+      <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+
+      <div>
+        Small plates, salads & sandwiches - an intimate setting with 12 indoor
+        seats plus patio seating.
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "TheProjectListItem",
+};
+</script>
+
+<style lang="scss" scope>
+.project-item-card {
+  width: 374px;
+  height: 440px;
+}
+</style>
