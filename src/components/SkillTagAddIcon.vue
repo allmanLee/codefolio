620d54fffeb6a842c6d7a14088bfa370581f33c9
이유준 사원 <yjlee@seowoninfo.com>
2022-05-26 00:23:17 +0900

    markup: 프로젝트 생성부분 마크업

--- src/components/SkillTagAddIcon.vue
+++ src/components/SkillTagAddIcon.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <button>+</button>
+  <div class="skill-tag-add-icon">
+    <button class="plus-button"><i class="plus-icon"></i></button>
   </div>
 </template>
 
@@ -10,4 +10,30 @@ export default {
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.skill-tag-add-icon {
+  display: inline-block;
+  .plus-button {
+    display: inline-flex;
+    box-sizing: border-box;
+    width: 40px;
+    height: 40px;
+    border: 1px solid white;
+    border-radius: 4px;
+    justify-content: center;
+    background: url("../assets/icon/icon_plus.png") no-repeat;
+    background-position: center center;
+    .plus-icon {
+      display: flex;
+      align-self: center;
+      margin-top: -2px;
+    }
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+    &:active {
+      background-color: rgba(255, 255, 255, 0.4);
+    }
+  }
+}
+</style>
---
+++ src/components/common/list/AppRegisterContent.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="app-register-content">
+    <div v-if="$slots.default" class="custom-register-content">
+      <slot></slot>
+    </div>
+    <div v-else class="register-content">
+      <h2>{{ label }}</h2>
+      <textarea
+        v-if="inputType === 'textarea'"
+        class="register-content-textarea"
+        :placeholder="placeholder"
+        maxlength="200"
+        rows="5"
+        :value="value"
+      ></textarea>
+      <input
+        v-else
+        class="register-content-input"
+        :type="inputType"
+        :placeholder="placeholder"
+        :value="value"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: "value",
+    event: "change",
+  },
+  props: {
+    label: {
+      type: String,
+      default: "",
+    },
+    inputType: {
+      type: String,
+      default: "text",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+    placeholder: {
+      type: String,
+      default: "입력해주세요.",
+    },
+  },
+  computed: {
+    valueLength() {
+      return this.value.length;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.app-register-content {
+  margin-bottom: 24px;
+  .register-content-input,
+  .register-content-textarea {
+    width: 100%;
+    margin-top: 16px;
+    color: white;
+    outline: none;
+    border-radius: 12px;
+    background-color: rgba(0, 0, 0, 0.24);
+    box-sizing: border-box;
+    padding: {
+      left: 20px;
+      right: 20px;
+      top: 10px;
+      bottom: 10px;
+    }
+    &:focus {
+      outline: 1px solid rgba(255, 255, 255, 1);
+      transition: all 0.3s ease-in-out;
+    }
+    &:hover {
+      outline: 1px solid rgba(255, 255, 255, 0.8);
+    }
+  }
+  .register-content-input {
+    height: rem-calc(40px);
+  }
+  .register-content-textarea {
+    resize: none;
+  }
+}
+</style>
